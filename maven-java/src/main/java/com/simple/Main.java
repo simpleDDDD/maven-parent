b24a038d37dae626662bf77d29650771040a2897
wwj <15858176843@163.com>
2021-11-15 21:06:08 +0800

    update Main.java by simple

--- maven-java/src/main/java/com/simple/Main.java
+++ maven-java/src/main/java/com/simple/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("哈哈");
         System.out.println("update by GitHub.com");
+        System.out.println("update by IDEA");
     }
 }
